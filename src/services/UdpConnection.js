dc81a3953245fe0c4eb1ab5152b0d3fd46a46435
Marcin Fortuniak <fortgeek@gmail.com>
2022-06-09 23:33:17 +0200

    message parsing

--- src/services/UdpConnection.js
+++ src/services/UdpConnection.js
@@ -1,15 +1,23 @@
 import UdpSockets from 'react-native-udp';
+import { NetworkInfo } from "react-native-network-info";
 
 const runSocket = async () => {
   const socket = UdpSockets.createSocket({type: 'udp4'});
   console.log('running socket');
   socket.bind(6969);
   socket.on('message', function (msg, rinfo) {
-    console.log(
-      'Message received',
-      String.fromCharCode.apply(null, new Uint8Array(msg)),
-    );
+    handleMessage(msg);
   });
+};
+
+const handleMessage = message => {
+  const messageContent = String.fromCharCode.apply(
+    null,
+    new Uint8Array(message),
+  );
+  const msg = JSON.parse(messageContent);
+  console.log('Message received', messageContent);
+  console.log(msg.event);
 };
 
 export default runSocket;
