8c615c2b2abc87cb12d6748bfb17a11af44923c9
Marcin Fortuniak <fortgeek@gmail.com>
2022-06-11 00:09:59 +0200

    confirming connection with mobile

--- src/reducers/leagueReducer.js
+++ src/reducers/leagueReducer.js
@@ -3,14 +3,24 @@ import {INCOMING_CONNECTION} from '../constants/redux';
 const initialState = {
   connected: false,
   step: 0,
+  desktopHost: '',
 };
 const leagueReducer = (state = initialState, message) => {
-  switch (message.event) {
+  switch (message.type) {
     case INCOMING_CONNECTION:
+      console.log(message.address + ':2138/connection');
+      fetch('http://' + message.address + ':2138/connection', {
+        method: 'POST',
+        body: JSON.stringify({
+          host: '192.168.1.1',
+        }),
+      });
+      console.log('Connection made.');
       return {
         ...state,
         connected: true,
         step: 1,
+        desktopHost: message.address,
       };
     default:
       return state;
